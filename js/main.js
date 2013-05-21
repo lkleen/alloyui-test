4a47c5af67b8c8c9794bd20e5cb4c40be6d1b877
Lars Kleen <lars.kleen@retrosounds.de>
2013-05-21 13:18:27 +0200

    running yui example

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+// Create a new YUI instance and populate it with the required modules.
+YUI().use('app', function (Y) {
 
+    var app = new Y.App();
+    var view = new Y.View();
+
+    view.render = function() {
+        this.get('container').set('text', 'hello yui world');
+        return this;
+    }
+
+    app.render().showView(view, null, {render: true});
+
+    Y.log(app.get('activeView') === view);
+    Y.log(app.get('viewContainer').contains(view.get('container')));
+
+});
